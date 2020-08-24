e07f9c48700e81d05b1cc79a308facb454d1d8ab
Ian Vasco <ianesvasco@gmail.com>
2020-08-23 22:00:41 -0500

    creates history screen

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,18 +1,54 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
+import { Container, Content } from 'native-base';
+import CustomHeader from '../../components/Header';
+import { StackNavigationProp } from '@react-navigation/stack';
+import LyricPreview from '../../components/LyricPreview';
+import { useStore } from '../../store';
 
-interface HistoryProps {}
+interface HistoryProps {
+    navigation: StackNavigationProp<any>
+}
 
-const History = (props: HistoryProps) => {
+const History = ({navigation}: HistoryProps) => {
+
+  const {historyState} = useStore()
+
   return (
-    <View style={styles.container}>
-      <Text>History</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Container>
+      <CustomHeader title="History" navigation={navigation} />
+      <Content style={styles.contentContainer}>
+        <FlatList 
+            data={historyState}
+            keyExtractor={(item, i) => `${item.title}-${item.artist}-${i}`}
+            renderItem={({item}) => <LyricPreview song={item} navigation={navigation} />
+} />
+      </Content>
+    </Container>
+  </SafeAreaView>
   );
 };
 
 export default History;
 
 const styles = StyleSheet.create({
-  container: {}
-});
+    buttonText: {
+      color: 'white',
+      fontWeight: '700'
+    },
+    container: {
+      flex: 1,
+      backgroundColor: '#fafafa',
+    },
+    contentContainer: {
+      margin: 16
+    },
+    historyTitle: {
+      fontSize: 20,
+      fontWeight: '700'
+    },
+    searchButton: {
+      marginTop: 30
+    },
+  });
